44fa065f38b1ae7dc4a818591ba5688d33ca71be
snitxmhm <snitxmhm@gmail.com>
2021-10-09 08:59:09 +0800

    square texture

--- main.cpp
+++ main.cpp
@@ -5,26 +5,32 @@
  * Author: Einsam
  * Copyright (C): 2021 All rights reserved
  */
+#define STB_IMAGE_IMPLEMENTATION
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <math.h>
+#include "stb_image.h"
 
 #include "./shader.h"
 
 using namespace std;
 void processInput(GLFWwindow* window);
 
-// triangle vertices.
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+// square vertices.
 float vertices[] = {
-    // 位置              // 颜色
-     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
-    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
-     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+    // 位置              // 颜色            // 纹理坐标
+     0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   1.0f, 1.0f,// 右上
+     0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,// 右下
+     -0.5f,-0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   0.0f, 0.0f,// 左下
+     -0.5f, 0.5f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f,// 左上
 };
 
 unsigned int indices[] = {
-    0, 1, 2
+    0, 1, 3,
+    1, 2, 3,
 };
 
 int main(int argc, const char *argv[])
@@ -40,7 +46,7 @@ int main(int argc, const char *argv[])
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     // Open a window and create its context
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Solar System", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Solar System", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -68,7 +74,6 @@ int main(int argc, const char *argv[])
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     Shader ourShader("./shader.vs", "./shader.fs");
-    ourShader.use();
 
     // create VBO and bind buffer to  VBO
     // create VAO and bind VBO to VAO
@@ -88,12 +93,47 @@ int main(int argc, const char *argv[])
     //use glDrawElements instead to glDrawArrays
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+    // create texture object
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // 为当前绑定的纹理对象设置环绕, 过滤方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // 放大与缩小时过滤方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load picture
+    int width, height, nrChannels;
+    // stbi_load use width, height, nrChannels of image
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+
+    // GL_TEXTURE_2D 生成与当前绑定的纹理对象在同一目标上的纹理
+    // 第二个参数为纹理指定多级渐远纹理的级别
+    // 第三个参数为把纹理存储为何种格式, RGB
+    // 纹理宽度和高度
+    if(data)
+    {
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
     // location attributes
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // color attributes
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    // texture attributes
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
     // Enable vertex attributes. Default disabled.
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     // Bind location 0 to Vertex Array.
@@ -105,13 +145,9 @@ int main(int argc, const char *argv[])
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Change color according to time through uniform buffer
-        float timeValue = glfwGetTime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        // use glGetUniformLocation to search the location of the ourColor.
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-        ourShader.setColor("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        ourShader.use();
 
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
--- shader.fs
+++ shader.fs
@@ -1,8 +1,14 @@
 #version 330 core
   out vec4 FragColor;
   in vec3 ourColor;
+  in vec2 TexCoord;
+
+  // GLSL有一个供纹理对象使用的内建数据类型, 叫做采样器, 以纹理类型作为后缀
+  uniform sampler2D ourTexture;
+
   void main()
   {
-    FragColor = vec4(ourColor, 1.0);
+    //FragColor = vec4(ourColor, 1.0);
+    FragColor = texture2D(ourTexture, TexCoord);
   };
 
