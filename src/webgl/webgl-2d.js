3e7bc68c6973150d504ab4064f6b01b8b26b8bbc
snitxmhm <snitxmhm@gmail.com>
2021-10-16 15:53:25 +0800

    二维矩阵

--- src/webgl/webgl-2d.js
+++ src/webgl/webgl-2d.js
@@ -6,21 +6,12 @@ in vec2 a_position;
 
 // Used to pass in the resolution of the canvas
 uniform vec2 u_resolution;
-
-uniform vec2 u_translation;
-uniform vec2 u_rotation;
-uniform vec2 u_scale;
+uniform mat3 u_matrix;
 
 // all shaders have a main function
 void main() {
-  // 缩放
-  vec2 scaledPosition = a_position * u_scale;
-   // 旋转位置
-  vec2 rotatedPosition = vec2(scaledPosition.x * u_rotation.y + scaledPosition.y * u_rotation.x,
-                              scaledPosition.y * u_rotation.y - scaledPosition.x * u_rotation.x);
-
-  // 加上平移量
-  vec2 position = rotatedPosition + u_translation;
+  // 顶点坐标乘以矩阵
+  vec2 position = (u_matrix * vec3(a_position, 1)).xy;
 
   // convert the position from pixels to 0.0 to 1.0
   vec2 zeroToOne = position / u_resolution;
@@ -49,6 +40,55 @@ void main() {
 }
 `;
 
+var m3 = {
+  translation: function (tx, ty) {
+    return [1, 0, 0, 0, 1, 0, tx, ty, 1];
+  },
+
+  rotation: function (angleInRadians) {
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+    return [c, -s, 0, s, c, 0, 0, 0, 1];
+  },
+
+  scaling: function (sx, sy) {
+    return [sx, 0, 0, 0, sy, 0, 0, 0, 1];
+  },
+
+  multiply: function (a, b) {
+    var a00 = a[0 * 3 + 0];
+    var a01 = a[0 * 3 + 1];
+    var a02 = a[0 * 3 + 2];
+    var a10 = a[1 * 3 + 0];
+    var a11 = a[1 * 3 + 1];
+    var a12 = a[1 * 3 + 2];
+    var a20 = a[2 * 3 + 0];
+    var a21 = a[2 * 3 + 1];
+    var a22 = a[2 * 3 + 2];
+    var b00 = b[0 * 3 + 0];
+    var b01 = b[0 * 3 + 1];
+    var b02 = b[0 * 3 + 2];
+    var b10 = b[1 * 3 + 0];
+    var b11 = b[1 * 3 + 1];
+    var b12 = b[1 * 3 + 2];
+    var b20 = b[2 * 3 + 0];
+    var b21 = b[2 * 3 + 1];
+    var b22 = b[2 * 3 + 2];
+
+    return [
+      b00 * a00 + b01 * a10 + b02 * a20,
+      b00 * a01 + b01 * a11 + b02 * a21,
+      b00 * a02 + b01 * a12 + b02 * a22,
+      b10 * a00 + b11 * a10 + b12 * a20,
+      b10 * a01 + b11 * a11 + b12 * a21,
+      b10 * a02 + b11 * a12 + b12 * a22,
+      b20 * a00 + b21 * a10 + b22 * a20,
+      b20 * a01 + b21 * a11 + b22 * a21,
+      b20 * a02 + b21 * a12 + b22 * a22,
+    ];
+  },
+};
+
 function main() {
   // Get A WebGL context
   /** @type {HTMLCanvasElement} */
@@ -73,9 +113,7 @@ function main() {
     "u_resolution"
   );
   var colorLocation = gl.getUniformLocation(program, "u_color");
-  var translationLocation = gl.getUniformLocation(program, "u_translation");
-  var rotationLocation = gl.getUniformLocation(program, "u_rotation");
-  var scaleLocation = gl.getUniformLocation(program, "u_scale");
+  var matrixLocation = gl.getUniformLocation(program, "u_matrix");
 
   // Create a buffer
   var positionBuffer = gl.createBuffer();
@@ -108,7 +146,7 @@ function main() {
   );
 
   var translation = [100, 150];
-  var rotation = [0, 1];
+  var rotationInRadians = 0;
   var scale = [1, 1];
   var color = [Math.random(), Math.random(), Math.random(), 1];
 
@@ -152,9 +190,7 @@ function main() {
 
   function updateAngle(event, ui) {
     var angleInDegrees = 360 - ui.value;
-    var angleInRadians = (angleInDegrees * Math.PI) / 180;
-    rotation[0] = Math.sin(angleInRadians);
-    rotation[1] = Math.cos(angleInRadians);
+    rotationInRadians = (angleInDegrees * Math.PI) / 180;
     drawScene();
   }
 
@@ -192,14 +228,17 @@ function main() {
     // Set a random color.
     gl.uniform4fv(colorLocation, color);
 
-    // Set translation
-    gl.uniform2fv(translationLocation, translation);
+    // calculate matrix
+    var translationMatrix = m3.translation(translation[0], translation[1]);
+    var rotationMatrix = m3.rotation(rotationInRadians);
+    var scaleMatrix = m3.scaling(scale[0], scale[1]);
 
-    // Set rotation
-    gl.uniform2fv(rotationLocation, rotation);
+    // multiply matrix
+    var matrix = m3.multiply(translationMatrix, rotationMatrix);
+    matrix = m3.multiply(matrix, scaleMatrix);
 
-    // Set scale
-    gl.uniform2fv(scaleLocation, scale);
+    // Set matrix
+    gl.uniformMatrix3fv(matrixLocation, false, matrix);
 
     // Draw the rectangle.
     var primitiveType = gl.TRIANGLES;
