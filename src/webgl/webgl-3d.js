f9744ddad2a6d75a17cd47106f2780ee5d054768
snitxmhm <snitxmhm@gmail.com>
2021-10-17 14:48:15 +0800

    三维相机

--- src/webgl/webgl-3d.js
+++ src/webgl/webgl-3d.js
@@ -6,20 +6,13 @@ in vec4 a_position;
 in vec4 a_color;
 
 uniform mat4 u_matrix;
-uniform float u_fudgeFactor;
 
 // 传递给片段着色器的颜色变量
 out vec4 v_color;
 
 // all shaders have a main function
 void main() {
-  vec4 position = u_matrix * a_position;
-
-  // 调整除数 因为裁剪空间中的Z值是一个较小的值(-1, 1)
-  float zToDivideBy = 1.0 + position.z * u_fudgeFactor;
-
-  // x , y, z 除以zToDivideBy
-  gl_Position = vec4(position.xyz, zToDivideBy);
+  gl_Position = u_matrix * a_position;
 
   v_color = a_color;
 }
@@ -39,7 +32,58 @@ void main() {
 }
 `;
 
+function radToDeg(r) {
+  return (r * 180) / Math.PI;
+}
+
+function degToRad(d) {
+  return (d * Math.PI) / 180;
+}
+
+function cross(a, b) {
+  return [
+    a[1] * b[2] - a[2] * b[1],
+    a[2] * b[0] - a[0] * b[2],
+    a[0] * b[1] - a[1] * b[0],
+  ];
+}
+function subtractVectors(a, b) {
+  return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+}
+function normalize(v) {
+  var length = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+  // 确定不会除以 0
+  if (length > 0.00001) {
+    return [v[0] / length, v[1] / length, v[2] / length];
+  } else {
+    return [0, 0, 0];
+  }
+}
 var m4 = {
+  lookAt: function (cameraPosition, target, up) {
+    var zAxis = normalize(subtractVectors(cameraPosition, target));
+    var xAxis = normalize(cross(up, zAxis));
+    var yAxis = normalize(cross(zAxis, xAxis));
+
+    return [
+      xAxis[0],
+      xAxis[1],
+      xAxis[2],
+      0,
+      yAxis[0],
+      yAxis[1],
+      yAxis[2],
+      0,
+      zAxis[0],
+      zAxis[1],
+      zAxis[2],
+      0,
+      cameraPosition[0],
+      cameraPosition[1],
+      cameraPosition[2],
+      1,
+    ];
+  },
   identity: function () {
     return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
   },
@@ -70,14 +114,16 @@ var m4 = {
     return [sx, 0, 0, 0, 0, sy, 0, 0, 0, 0, sz, 0, 0, 0, 0, 1];
   },
 
-  projection: function (width, height, depth) {
-    // 翻转Y轴
+  perspective: function (fieldOfViewInRadians, aspect, near, far) {
+    var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
+    var rangeInv = 1.0 / (near - far);
+
     // prettier-ignore
     return [
-      2 / width, 0, 0, 0,
-      0, -2 / height, 0, 0,
-      0, 0, 2 / depth, 0,
-      -1, 1, 0, 1,
+      f / aspect, 0, 0, 0,
+      0, f, 0, 0,
+      0, 0, (near + far) * rangeInv, -1,
+      0, 0, near * far * rangeInv * 2, 0
     ];
   },
 
@@ -154,6 +200,148 @@ var m4 = {
       b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
     ];
   },
+  inverse: function (m) {
+    var m00 = m[0 * 4 + 0];
+    var m01 = m[0 * 4 + 1];
+    var m02 = m[0 * 4 + 2];
+    var m03 = m[0 * 4 + 3];
+    var m10 = m[1 * 4 + 0];
+    var m11 = m[1 * 4 + 1];
+    var m12 = m[1 * 4 + 2];
+    var m13 = m[1 * 4 + 3];
+    var m20 = m[2 * 4 + 0];
+    var m21 = m[2 * 4 + 1];
+    var m22 = m[2 * 4 + 2];
+    var m23 = m[2 * 4 + 3];
+    var m30 = m[3 * 4 + 0];
+    var m31 = m[3 * 4 + 1];
+    var m32 = m[3 * 4 + 2];
+    var m33 = m[3 * 4 + 3];
+    var tmp_0 = m22 * m33;
+    var tmp_1 = m32 * m23;
+    var tmp_2 = m12 * m33;
+    var tmp_3 = m32 * m13;
+    var tmp_4 = m12 * m23;
+    var tmp_5 = m22 * m13;
+    var tmp_6 = m02 * m33;
+    var tmp_7 = m32 * m03;
+    var tmp_8 = m02 * m23;
+    var tmp_9 = m22 * m03;
+    var tmp_10 = m02 * m13;
+    var tmp_11 = m12 * m03;
+    var tmp_12 = m20 * m31;
+    var tmp_13 = m30 * m21;
+    var tmp_14 = m10 * m31;
+    var tmp_15 = m30 * m11;
+    var tmp_16 = m10 * m21;
+    var tmp_17 = m20 * m11;
+    var tmp_18 = m00 * m31;
+    var tmp_19 = m30 * m01;
+    var tmp_20 = m00 * m21;
+    var tmp_21 = m20 * m01;
+    var tmp_22 = m00 * m11;
+    var tmp_23 = m10 * m01;
+
+    var t0 =
+      tmp_0 * m11 +
+      tmp_3 * m21 +
+      tmp_4 * m31 -
+      (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
+    var t1 =
+      tmp_1 * m01 +
+      tmp_6 * m21 +
+      tmp_9 * m31 -
+      (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
+    var t2 =
+      tmp_2 * m01 +
+      tmp_7 * m11 +
+      tmp_10 * m31 -
+      (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
+    var t3 =
+      tmp_5 * m01 +
+      tmp_8 * m11 +
+      tmp_11 * m21 -
+      (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+
+    var d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+
+    return [
+      d * t0,
+      d * t1,
+      d * t2,
+      d * t3,
+      d *
+        (tmp_1 * m10 +
+          tmp_2 * m20 +
+          tmp_5 * m30 -
+          (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
+      d *
+        (tmp_0 * m00 +
+          tmp_7 * m20 +
+          tmp_8 * m30 -
+          (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
+      d *
+        (tmp_3 * m00 +
+          tmp_6 * m10 +
+          tmp_11 * m30 -
+          (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
+      d *
+        (tmp_4 * m00 +
+          tmp_9 * m10 +
+          tmp_10 * m20 -
+          (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
+      d *
+        (tmp_12 * m13 +
+          tmp_15 * m23 +
+          tmp_16 * m33 -
+          (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
+      d *
+        (tmp_13 * m03 +
+          tmp_18 * m23 +
+          tmp_21 * m33 -
+          (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
+      d *
+        (tmp_14 * m03 +
+          tmp_19 * m13 +
+          tmp_22 * m33 -
+          (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
+      d *
+        (tmp_17 * m03 +
+          tmp_20 * m13 +
+          tmp_23 * m23 -
+          (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
+      d *
+        (tmp_14 * m22 +
+          tmp_17 * m32 +
+          tmp_13 * m12 -
+          (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
+      d *
+        (tmp_20 * m32 +
+          tmp_12 * m02 +
+          tmp_19 * m22 -
+          (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
+      d *
+        (tmp_18 * m12 +
+          tmp_23 * m32 +
+          tmp_15 * m02 -
+          (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
+      d *
+        (tmp_22 * m22 +
+          tmp_16 * m02 +
+          tmp_21 * m12 -
+          (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02)),
+    ];
+  },
+  transformVector: function (m, v) {
+    var dst = [];
+    for (var i = 0; i < 4; ++i) {
+      dst[i] = 0.0;
+      for (var j = 0; j < 4; ++j) {
+        dst[i] += v[j] * m[j * 4 + i];
+      }
+    }
+    return dst;
+  },
 };
 
 function main() {
@@ -177,7 +365,6 @@ function main() {
 
   // look up uniform locations
   var matrixLocation = gl.getUniformLocation(program, "u_matrix");
-  var fudgeLocation = gl.getUniformLocation(program, "u_fudgeFactor");
 
   // Create a vertex array object (attribute state)
   var vao = gl.createVertexArray();
@@ -233,91 +420,28 @@ function main() {
     offset
   );
 
-  var translation = [0, 0, 0];
-  var rotation = [0, 0, 0];
-  var scale = [1, 1, 1];
-  var fudgeFactor = 1;
+  var fieldOfViewRadians = degToRad(60);
+  var cameraAngleRadians = degToRad(0);
 
   drawScene();
 
   // Setup a ui.
-  webglLessonsUI.setupSlider("#fudgeFactor", {
-    slide: updateFudgeFactor,
-    max: 2,
-    step: 0.01,
-    precision: 2,
-  });
-  webglLessonsUI.setupSlider("#x", {
-    slide: updatePosition(0),
-    max: gl.canvas.width,
-  });
-  webglLessonsUI.setupSlider("#y", {
-    slide: updatePosition(1),
-    max: gl.canvas.height,
-  });
-  webglLessonsUI.setupSlider("#z", {
-    slide: updatePosition(2),
-    max: gl.canvas.height,
+  webglLessonsUI.setupSlider("#cameraAngle", {
+    value: radToDeg(cameraAngleRadians),
+    slide: updateCameraAngle,
+    min: -360,
+    max: 360,
   });
 
-  webglLessonsUI.setupSlider("#angleX", { slide: updateAngle(0), max: 360 });
-  webglLessonsUI.setupSlider("#angleY", { slide: updateAngle(1), max: 360 });
-  webglLessonsUI.setupSlider("#angleZ", { slide: updateAngle(2), max: 360 });
-
-  webglLessonsUI.setupSlider("#scaleX", {
-    value: scale[0],
-    slide: updateScale(0),
-    min: -5,
-    max: 5,
-    step: 0.01,
-    precision: 2,
-  });
-  webglLessonsUI.setupSlider("#scaleY", {
-    value: scale[1],
-    slide: updateScale(1),
-    min: -5,
-    max: 5,
-    step: 0.01,
-    precision: 2,
-  });
-  webglLessonsUI.setupSlider("#scaleZ", {
-    value: scale[1],
-    slide: updateScale(1),
-    min: -5,
-    max: 5,
-    step: 0.01,
-    precision: 2,
-  });
-
-  function updateFudgeFactor(event, ui) {
-    fudgeFactor = ui.value;
+  function updateCameraAngle(event, ui) {
+    cameraAngleRadians = degToRad(ui.value);
     drawScene();
   }
 
-  function updatePosition(index) {
-    return function (event, ui) {
-      translation[index] = ui.value;
-      drawScene();
-    };
-  }
-
-  function updateAngle(index) {
-    return function (event, ui) {
-      var angleInDegrees = 360 - ui.value;
-      var rotationInRadians = (angleInDegrees * Math.PI) / 180;
-      rotation[index] = rotationInRadians;
-      drawScene();
-    };
-  }
-
-  function updateScale(index) {
-    return function (event, ui) {
-      scale[index] = ui.value;
-      drawScene();
-    };
-  }
-
   function drawScene() {
+    var numFs = 5;
+    var radius = 200;
+
     webglUtils.resizeCanvasToDisplaySize(gl.canvas);
 
     // 剔除背面三角形绘制
@@ -339,90 +463,131 @@ function main() {
     // Bind the attribute/buffer set we want.
     gl.bindVertexArray(vao);
 
+    var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    var zNear = 1;
+    var zFar = 2000;
     // calculate matrix
-    var matrix = m4.projection(
-      gl.canvas.clientWidth,
-      gl.canvas.clientHeight,
-      400
+    var projectionMatrix = m4.perspective(
+      fieldOfViewRadians,
+      aspect,
+      zNear,
+      zFar
     );
-    matrix = m4.translate(
-      matrix,
-      translation[0],
-      translation[1],
-      translation[2]
-    );
-    matrix = m4.xRotate(matrix, rotation[0]);
-    matrix = m4.yRotate(matrix, rotation[1]);
-    matrix = m4.zRotate(matrix, rotation[2]);
-    matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
 
-    gl.uniformMatrix4fv(matrixLocation, false, matrix);
+    // 计算第一个F的位置
+    var fPosition = [radius, 0, 0];
 
-    // set fudgeFactor
-    gl.uniform1f(fudgeLocation, fudgeFactor);
-    // Draw the rectangle.
-    var primitiveType = gl.TRIANGLES;
-    var offset = 0;
-    var count = 16 * 6;
-    gl.drawArrays(primitiveType, offset, count);
+    // 计算相机再圆上的位置矩阵
+    var cameraMatrix = m4.yRotation(cameraAngleRadians);
+    cameraMatrix = m4.translate(cameraMatrix, 0, 50, radius * 1.5);
+
+    // 获得矩阵中相机的位置
+    var cameraPosition = [cameraMatrix[12], cameraMatrix[13], cameraMatrix[14]];
+
+    var up = [0, 1, 0];
+
+    // 计算相机的朝向矩阵
+    var cameraMatrix = m4.lookAt(cameraPosition, fPosition, up);
+
+    // 通过相机矩阵计算视图矩阵
+    var viewMatrix = m4.inverse(cameraMatrix);
+
+    var viewMatrix = m4.inverse(cameraMatrix);
+
+    // Make a view matrix from the camera matrix.
+
+    // 将投影空间移动到视图空间 (摄像机前方的空间)
+    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
+
+    // 绘制一圈'F'
+    for (var ii = 0; ii < numFs; ++ii) {
+      var angle = (ii * Math.PI * 2) / numFs;
+
+      var x = Math.cos(angle) * radius;
+      var z = Math.sin(angle) * radius;
+      // 从视图投影矩阵开始
+      // 计算 F 矩阵
+      var matrix = m4.translate(viewProjectionMatrix, x, 0, z);
+
+      // Set matrix
+      gl.uniformMatrix4fv(matrixLocation, false, matrix);
+
+      // draw
+      var primitiveType = gl.TRIANGLES;
+      var offset = 0;
+      var count = 16 * 6;
+      gl.drawArrays(primitiveType, offset, count);
+    }
   }
 }
 // Fill the current ARRAY_BUFFER buffer
 // with the values that define a letter 'F'.
 function setGeometry(gl) {
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([
-      // left column front
-      0, 0, 0, 0, 150, 0, 30, 0, 0, 0, 150, 0, 30, 150, 0, 30, 0, 0,
+  var positions = new Float32Array([
+    // left column front
+    0, 0, 0, 0, 150, 0, 30, 0, 0, 0, 150, 0, 30, 150, 0, 30, 0, 0,
 
-      // top rung front
-      30, 0, 0, 30, 30, 0, 100, 0, 0, 30, 30, 0, 100, 30, 0, 100, 0, 0,
+    // top rung front
+    30, 0, 0, 30, 30, 0, 100, 0, 0, 30, 30, 0, 100, 30, 0, 100, 0, 0,
 
-      // middle rung front
-      30, 60, 0, 30, 90, 0, 67, 60, 0, 30, 90, 0, 67, 90, 0, 67, 60, 0,
+    // middle rung front
+    30, 60, 0, 30, 90, 0, 67, 60, 0, 30, 90, 0, 67, 90, 0, 67, 60, 0,
 
-      // left column back
-      0, 0, 30, 30, 0, 30, 0, 150, 30, 0, 150, 30, 30, 0, 30, 30, 150, 30,
+    // left column back
+    0, 0, 30, 30, 0, 30, 0, 150, 30, 0, 150, 30, 30, 0, 30, 30, 150, 30,
 
-      // top rung back
-      30, 0, 30, 100, 0, 30, 30, 30, 30, 30, 30, 30, 100, 0, 30, 100, 30, 30,
+    // top rung back
+    30, 0, 30, 100, 0, 30, 30, 30, 30, 30, 30, 30, 100, 0, 30, 100, 30, 30,
 
-      // middle rung back
-      30, 60, 30, 67, 60, 30, 30, 90, 30, 30, 90, 30, 67, 60, 30, 67, 90, 30,
+    // middle rung back
+    30, 60, 30, 67, 60, 30, 30, 90, 30, 30, 90, 30, 67, 60, 30, 67, 90, 30,
 
-      // top
-      0, 0, 0, 100, 0, 0, 100, 0, 30, 0, 0, 0, 100, 0, 30, 0, 0, 30,
+    // top
+    0, 0, 0, 100, 0, 0, 100, 0, 30, 0, 0, 0, 100, 0, 30, 0, 0, 30,
 
-      // top rung right
-      100, 0, 0, 100, 30, 0, 100, 30, 30, 100, 0, 0, 100, 30, 30, 100, 0, 30,
+    // top rung right
+    100, 0, 0, 100, 30, 0, 100, 30, 30, 100, 0, 0, 100, 30, 30, 100, 0, 30,
 
-      // under top rung
-      30, 30, 0, 30, 30, 30, 100, 30, 30, 30, 30, 0, 100, 30, 30, 100, 30, 0,
+    // under top rung
+    30, 30, 0, 30, 30, 30, 100, 30, 30, 30, 30, 0, 100, 30, 30, 100, 30, 0,
 
-      // between top rung and middle
-      30, 30, 0, 30, 60, 30, 30, 30, 30, 30, 30, 0, 30, 60, 0, 30, 60, 30,
+    // between top rung and middle
+    30, 30, 0, 30, 60, 30, 30, 30, 30, 30, 30, 0, 30, 60, 0, 30, 60, 30,
 
-      // top of middle rung
-      30, 60, 0, 67, 60, 30, 30, 60, 30, 30, 60, 0, 67, 60, 0, 67, 60, 30,
+    // top of middle rung
+    30, 60, 0, 67, 60, 30, 30, 60, 30, 30, 60, 0, 67, 60, 0, 67, 60, 30,
 
-      // right of middle rung
-      67, 60, 0, 67, 90, 30, 67, 60, 30, 67, 60, 0, 67, 90, 0, 67, 90, 30,
+    // right of middle rung
+    67, 60, 0, 67, 90, 30, 67, 60, 30, 67, 60, 0, 67, 90, 0, 67, 90, 30,
 
-      // bottom of middle rung.
-      30, 90, 0, 30, 90, 30, 67, 90, 30, 30, 90, 0, 67, 90, 30, 67, 90, 0,
+    // bottom of middle rung.
+    30, 90, 0, 30, 90, 30, 67, 90, 30, 30, 90, 0, 67, 90, 30, 67, 90, 0,
 
-      // right of bottom
-      30, 90, 0, 30, 150, 30, 30, 90, 30, 30, 90, 0, 30, 150, 0, 30, 150, 30,
+    // right of bottom
+    30, 90, 0, 30, 150, 30, 30, 90, 30, 30, 90, 0, 30, 150, 0, 30, 150, 30,
 
-      // bottom
-      0, 150, 0, 0, 150, 30, 30, 150, 30, 0, 150, 0, 30, 150, 30, 30, 150, 0,
+    // bottom
+    0, 150, 0, 0, 150, 30, 30, 150, 30, 0, 150, 0, 30, 150, 30, 30, 150, 0,
 
-      // left side
-      0, 0, 0, 0, 0, 30, 0, 150, 30, 0, 0, 0, 0, 150, 30, 0, 150, 0,
-    ]),
-    gl.STATIC_DRAW
-  );
+    // left side
+    0, 0, 0, 0, 0, 30, 0, 150, 30, 0, 0, 0, 0, 150, 30, 0, 150, 0,
+  ]);
+  var matrix = m4.xRotation(Math.PI);
+  matrix = m4.translate(matrix, -50, -75, -15);
+
+  for (var ii = 0; ii < positions.length; ii += 3) {
+    var vector = m4.transformVector(matrix, [
+      positions[ii + 0],
+      positions[ii + 1],
+      positions[ii + 2],
+      1,
+    ]);
+    positions[ii + 0] = vector[0];
+    positions[ii + 1] = vector[1];
+    positions[ii + 2] = vector[2];
+  }
+
+  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
 }
 
 // Fill the current ARRAY_BUFFER buffer with colors for the 'F'.
@@ -497,5 +662,4 @@ function setColors(gl) {
     gl.STATIC_DRAW
   );
 }
-
 main();
